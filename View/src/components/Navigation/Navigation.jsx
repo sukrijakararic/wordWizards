c987cca6168ab411b29d08c4df513eba6042031f
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2025-02-07 20:24:53 +0100

    feat: enhance navigation component with routing and custom styles

--- View/src/components/Navigation/Navigation.jsx
+++ View/src/components/Navigation/Navigation.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import styles from "./Navigation.module.css";
+import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   return (
     <>
-      <Navbar bg="light" data-bs-theme="light">
+      <Navbar expand="lg" className="bg-body-tertiary" bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" className={styles.navLink} >wordWizards</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">tbd</Nav.Link>
-            <Nav.Link href="#features">tbd</Nav.Link>
-            <Nav.Link href="#pricing">tbd</Nav.Link>
+            <Nav.Link>
+              <Link to="/" className={styles.navLink}>
+                tbd
+              </Link>
+            </Nav.Link>
           </Nav>
+        </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
