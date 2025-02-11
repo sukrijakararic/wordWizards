4cd90a58449472eed0d840d33db3f897945c3d6e
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2025-02-11 15:17:07 +0100

    feat: restructure AboutUs component layout and add styling for content

--- View/src/components/AboutUs/AboutUs.jsx
+++ View/src/components/AboutUs/AboutUs.jsx
@@ -4,19 +4,24 @@ import styles from "./AboutUs.module.css";
 export const AboutUs = () => {
   return (
     <div className={styles.aboutContainer}>
-      <h1 className={styles.aboutTitle}> 📜 Welcome to WordWizards! ✨</h1>
-      <p className={styles.aboutText}>
-        At <span className={styles.highlight}>WordWizards</span>, Whether you're a seasoned
-        writer, an aspiring blogger, or just someone who loves a good read, you've found your haven.
-      </p>
-      <p className={styles.aboutText}>
-        Our mission? To weave stories, share knowledge, and spark conversations—one post at a time.
-      </p>
-      <p className={styles.aboutText}>
-        So grab your quill (or keyboard) and join us in fun and productive conversations! 📝✨
-      </p>
-      <div className={styles.aboutFooter}>
-        <p>📖 Happy Writing! — The WordWizards Team</p>
+      <div className={styles.aboutContent}>
+        <h1 className={styles.aboutTitle}> 📜 Welcome to WordWizards! ✨</h1>
+        <p className={styles.aboutText}>
+          At <span className={styles.highlight}>WordWizards</span>, Whether
+          you're a seasoned writer, an aspiring blogger, or just someone who
+          loves a good read, you've found your haven.
+        </p>
+        <p className={styles.aboutText}>
+          Our mission? To weave stories, share knowledge, and spark
+          conversations—one post at a time.
+        </p>
+        <p className={styles.aboutText}>
+          So grab your quill (or keyboard) and join us in fun and productive
+          conversations! 📝✨
+        </p>
+        <div className={styles.aboutFooter}>
+          <p>📖 Happy Writing! — The WordWizards Team</p>
+        </div>
       </div>
     </div>
   );
