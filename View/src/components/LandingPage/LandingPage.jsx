7653e12443a8997fdcfaff0e00792eac3986f55a
Sukrija Kararic <116309642+sukrijakararic@users.noreply.github.com>
2025-02-11 21:38:27 +0100

    feat: enhance LandingPage component with authentication check and conditional rendering

--- View/src/components/LandingPage/LandingPage.jsx
+++ View/src/components/LandingPage/LandingPage.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import styles from "./LandingPage.module.css";
 import { Registration } from "../Registration/Registration";
+import { AuthContext } from "../../context-api/AuthContext";
+import { Link } from "react-router-dom";
 
 export const LandingPage = () => {
-  return (
-    <div className={styles.landingPage}>
-      <div className={styles.title}>
-        <h1>
-          Welcome to <span className={styles.wordWizards}>wordWizards</span>
-        </h1>
-        <h3>Unleash the power of words!</h3>{" "}
+  const { loggedIn } = useContext(AuthContext);
+
+  if (loggedIn) {
+    return (
+      <div className={styles.landingPage}>
+        <div className={styles.title}>
+          <h1>
+            Welcome to <span className={styles.wordWizards}>wordWizards</span>
+          </h1>
+          <h3>Unleash the power of words!</h3>{" "}
+          <h5>Lets get to writing, <Link to="/createBlog" className={styles.shallWe}>shall we?</Link></h5>
+        </div>
       </div>
-      <Registration />
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={styles.landingPage}>
+        <div className={styles.title}>
+          <h1>
+            Welcome to <span className={styles.wordWizards}>wordWizards</span>
+          </h1>
+          <h3>Unleash the power of words!</h3>{" "}
+        </div>
+        <Registration />
+      </div>
+    );
+  }
 };
